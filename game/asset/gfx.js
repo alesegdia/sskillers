2310e6a7dd37553f0b3ecfd136f7739a04db4c10
Alejandro Seguí <alesegdia@gmail.com>
2014-12-02 04:45:22 +0100

    URLs fixed?

--- game/asset/gfx.js
+++ game/asset/gfx.js
@@ -3,86 +3,86 @@ SSK.namespace("SSK.game.gfx");
 SSK.game.gfx.SpriteCache = {};
 
 SSK.game.gfx.SpriteCache.SP_AFORCE = new Image();
-SSK.game.gfx.SpriteCache.SP_AFORCE.src = "../../media/img/aforce.png";
+SSK.game.gfx.SpriteCache.SP_AFORCE.src = "../media/img/aforce.png";
 
 SSK.game.gfx.SpriteCache.SP_ARMORBAR = new Image();
-SSK.game.gfx.SpriteCache.SP_ARMORBAR.src = "../../media/img/armorbar.png";
+SSK.game.gfx.SpriteCache.SP_ARMORBAR.src = "../media/img/armorbar.png";
 
 SSK.game.gfx.SpriteCache.SP_ARMORBAR_ = new Image();
-SSK.game.gfx.SpriteCache.SP_ARMORBAR_.src = "../../media/img/armorbar_.png";
+SSK.game.gfx.SpriteCache.SP_ARMORBAR_.src = "../media/img/armorbar_.png";
 
 SSK.game.gfx.SpriteCache.SP_BULLET = new Image();
-SSK.game.gfx.SpriteCache.SP_BULLET.src = "../../media/img/bullet.png";
+SSK.game.gfx.SpriteCache.SP_BULLET.src = "../media/img/bullet.png";
 
 SSK.game.gfx.SpriteCache.SP_CREDITS_0 = new Image();
-SSK.game.gfx.SpriteCache.SP_CREDITS_0.src = "../../media/img/credits-0.png";
+SSK.game.gfx.SpriteCache.SP_CREDITS_0.src = "../media/img/credits-0.png";
 
 SSK.game.gfx.SpriteCache.SP_CREDITS_1 = new Image();
-SSK.game.gfx.SpriteCache.SP_CREDITS_1.src = "../../media/img/credits-1.png";
+SSK.game.gfx.SpriteCache.SP_CREDITS_1.src = "../media/img/credits-1.png";
 
 SSK.game.gfx.SpriteCache.SP_DFORCE = new Image();
-SSK.game.gfx.SpriteCache.SP_DFORCE.src = "../../media/img/dforce.png";
+SSK.game.gfx.SpriteCache.SP_DFORCE.src = "../media/img/dforce.png";
 
 SSK.game.gfx.SpriteCache.SP_FIVEPX = new Image();
-SSK.game.gfx.SpriteCache.SP_FIVEPX.src = "../../media/img/fivepx.png";
+SSK.game.gfx.SpriteCache.SP_FIVEPX.src = "../media/img/fivepx.png";
 
 SSK.game.gfx.SpriteCache.SP_GAUGEBAR = new Image();
-SSK.game.gfx.SpriteCache.SP_GAUGEBAR.src = "../../media/img/gaugebar.png";
+SSK.game.gfx.SpriteCache.SP_GAUGEBAR.src = "../media/img/gaugebar.png";
 
 SSK.game.gfx.SpriteCache.SP_GENERATOR = new Image();
-SSK.game.gfx.SpriteCache.SP_GENERATOR.src = "../../media/img/generator.png";
+SSK.game.gfx.SpriteCache.SP_GENERATOR.src = "../media/img/generator.png";
 
 SSK.game.gfx.SpriteCache.SP_GUI = new Image();
-SSK.game.gfx.SpriteCache.SP_GUI.src = "../../media/img/gui.png";
+SSK.game.gfx.SpriteCache.SP_GUI.src = "../media/img/gui.png";
 
 SSK.game.gfx.SpriteCache.SP_HIGHSCORE_0 = new Image();
-SSK.game.gfx.SpriteCache.SP_HIGHSCORE_0.src = "../../media/img/highscore-0.png";
+SSK.game.gfx.SpriteCache.SP_HIGHSCORE_0.src = "../media/img/highscore-0.png";
 
 SSK.game.gfx.SpriteCache.SP_HIGHSCORE_1 = new Image();
-SSK.game.gfx.SpriteCache.SP_HIGHSCORE_1.src = "../../media/img/highscore-1.png";
+SSK.game.gfx.SpriteCache.SP_HIGHSCORE_1.src = "../media/img/highscore-1.png";
 
 SSK.game.gfx.SpriteCache.SP_INTROBG = new Image();
-SSK.game.gfx.SpriteCache.SP_INTROBG.src = "../../media/img/introbg.png";
+SSK.game.gfx.SpriteCache.SP_INTROBG.src = "../media/img/introbg.png";
 
 SSK.game.gfx.SpriteCache.SP_INTROSHIP = new Image();
-SSK.game.gfx.SpriteCache.SP_INTROSHIP.src = "../../media/img/introship.png";
+SSK.game.gfx.SpriteCache.SP_INTROSHIP.src = "../media/img/introship.png";
 
 SSK.game.gfx.SpriteCache.SP_MENUBG = new Image();
-SSK.game.gfx.SpriteCache.SP_MENUBG.src = "../../media/img/menubg.png";
+SSK.game.gfx.SpriteCache.SP_MENUBG.src = "../media/img/menubg.png";
 
 SSK.game.gfx.SpriteCache.SP_GEN = new Image();
-SSK.game.gfx.SpriteCache.SP_GEN.src = "../../media/img/astraslicer.png";
+SSK.game.gfx.SpriteCache.SP_GEN.src = "../media/img/astraslicer.png";
 
 SSK.game.gfx.SpriteCache.SP_NEWGAME_0 = new Image();
-SSK.game.gfx.SpriteCache.SP_NEWGAME_0.src = "../../media/img/newgame-0.png";
+SSK.game.gfx.SpriteCache.SP_NEWGAME_0.src = "../media/img/newgame-0.png";
 
 SSK.game.gfx.SpriteCache.SP_NEWGAME_1 = new Image();
-SSK.game.gfx.SpriteCache.SP_NEWGAME_1.src = "../../media/img/newgame-1.png";
+SSK.game.gfx.SpriteCache.SP_NEWGAME_1.src = "../media/img/newgame-1.png";
 
 SSK.game.gfx.SpriteCache.SP_SHIELDBAR = new Image();
-SSK.game.gfx.SpriteCache.SP_SHIELDBAR.src = "../../media/img/shieldbar.png";
+SSK.game.gfx.SpriteCache.SP_SHIELDBAR.src = "../media/img/shieldbar.png";
 
 SSK.game.gfx.SpriteCache.SP_SHIELDBAR_ = new Image();
-SSK.game.gfx.SpriteCache.SP_SHIELDBAR_.src = "../../media/img/shieldbar_.png";
+SSK.game.gfx.SpriteCache.SP_SHIELDBAR_.src = "../media/img/shieldbar_.png";
 
 SSK.game.gfx.SpriteCache.SP_SLIDE0 = new Image();
-SSK.game.gfx.SpriteCache.SP_SLIDE0.src = "../../media/img/slide0.png";
+SSK.game.gfx.SpriteCache.SP_SLIDE0.src = "../media/img/slide0.png";
 
 SSK.game.gfx.SpriteCache.SP_SLIDE1 = new Image();
-SSK.game.gfx.SpriteCache.SP_SLIDE1.src = "../../media/img/slide1.png";
+SSK.game.gfx.SpriteCache.SP_SLIDE1.src = "../media/img/slide1.png";
 
 SSK.game.gfx.SpriteCache.SP_SLIDE2 = new Image();
-SSK.game.gfx.SpriteCache.SP_SLIDE2.src = "../../media/img/slide2.png";
+SSK.game.gfx.SpriteCache.SP_SLIDE2.src = "../media/img/slide2.png";
 
 SSK.game.gfx.SpriteCache.SP_SLIDE3 = new Image();
-SSK.game.gfx.SpriteCache.SP_SLIDE3.src = "../../media/img/slide3.png";
+SSK.game.gfx.SpriteCache.SP_SLIDE3.src = "../media/img/slide3.png";
 
 SSK.game.gfx.SpriteCache.SP_SLIDE4 = new Image();
-SSK.game.gfx.SpriteCache.SP_SLIDE4.src = "../../media/img/slide4.png";
+SSK.game.gfx.SpriteCache.SP_SLIDE4.src = "../media/img/slide4.png";
 
 SSK.game.gfx.SpriteCache.SP_SLIDE5 = new Image();
-SSK.game.gfx.SpriteCache.SP_SLIDE5.src = "../../media/img/slide5.png";
+SSK.game.gfx.SpriteCache.SP_SLIDE5.src = "../media/img/slide5.png";
 
 SSK.game.gfx.SpriteCache.SP_SLIDE6 = new Image();
-SSK.game.gfx.SpriteCache.SP_SLIDE6.src = "../../media/img/slide6.png";
+SSK.game.gfx.SpriteCache.SP_SLIDE6.src = "../media/img/slide6.png";
 
